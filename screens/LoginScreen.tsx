9f017c8e486a2eff28fc6245e09720b85adb1778
Bryan1130A <leonaprot44@gmail.com>
2026-01-14 10:02:49 -0500

    cambioboton

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,13 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }: any) {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Register')}
+      >
+        <Text style={styles.buttonText}>Ir a Registro</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
-//hola
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#2563eb',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+})
